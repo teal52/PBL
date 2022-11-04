f7826649fcc438509067e47a33fc56609d3ec80a
teal52 <mys.alter10@gmail.com>
2022-11-04 09:31:22 +0900

    display weather information

--- original/weather.py
+++ original/weather.py
@@ -4,18 +4,33 @@ import requests
 
 url="https://www.jma.go.jp/bosai/forecast/data/forecast/080000.json"
 
-
+'''
 weather_data = requests.get(url).json()
 
 weather_date = weather_data[0]["timeSeries"][0]["timeDefines"][0]
 weather_weather = weather_data[0]["timeSeries"][0]["areas"][0]["weathers"][0]
+'''
+
+def get_json():
+    weather_data = requests.get(url).json()
+
+    weather_date = weather_data[0]["timeSeries"][0]["timeDefines"][0]
+    weather_weather = weather_data[0]["timeSeries"][0]["areas"][0]["weathers"][0]
+
+    weather_weather = weather_weather.replace('　','')
 
 
-weather_weather = weather_weather.replace('　','')
+    data_box={"date":weather_date,"weather":weather_weather}
+
+    return data_box
+
+'''
+a = get_json()
+print(a["weather"])
 
 
-
-print(weather_weather)
+weather_rainfall = weather_data["Feature"][0]["Property"]["WeatherList"]["Weather"][0]["Rainfall"]
+'''
 
 
 
