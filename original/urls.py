22b5c639660cacdb2a8fc168e40a2ce54710499f
teal52 <mys.alter10@gmail.com>
2022-11-06 01:21:49 +0900

    create setup page

--- original/urls.py
+++ original/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index),
-    path('entry/',views.entry),
+    path('',views.index,name='index'),
+    path('entry/',views.entry,name='entry'),
 ]
