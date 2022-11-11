3d3cf73c0db5e22ffb647982988af6f899f1401a
teal52 <mys.alter10@gmail.com>
2022-11-11 14:02:29 +0900

    write proxy

--- original/weather.py
+++ original/weather.py
@@ -11,6 +11,12 @@ weather_date = weather_data[0]["timeSeries"][0]["timeDefines"][0]
 weather_weather = weather_data[0]["timeSeries"][0]["areas"][0]["weathers"][0]
 '''
 
+proxies = {
+    'http':'http://po.cc.ibaraki-ct.ac.jp:3128',
+    'https':'http://po.cc.ibaraki-ct.ac.jp:3128'
+}
+
+
 def get_json():
     weather_data = requests.get(url).json()
 
