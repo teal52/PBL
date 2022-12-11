ec65a74da95bbf817388955cbf7958546955a9f5
teal3 <mys.alter10@gmail.com>
2022-12-11 16:58:25 +0900

    weather.py

--- original/weather.py
+++ original/weather.py
@@ -16,7 +16,7 @@ proxies = {
     'https':'http://po.cc.ibaraki-ct.ac.jp:3128'
 }
 
-
+'''
 def get_json():
     weather_data = requests.get(url,proxies=proxies).json()
 
@@ -29,6 +29,20 @@ def get_json():
     data_box={"date":weather_date,"weather":weather_weather}
 
     return data_box
+'''
+def get_json():
+    weather_data = requests.get(url).json()
+
+    weather_date = weather_data[0]["timeSeries"][0]["timeDefines"][0]
+    weather_weather = weather_data[0]["timeSeries"][0]["areas"][0]["weathers"][0]
+
+    weather_weather = weather_weather.replace('　','')
+
+
+    data_box={"date":weather_date,"weather":weather_weather}
+
+    return data_box
+
 
 '''
 a = get_json()
